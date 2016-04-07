f30d357a9900f0dcc56f1b7eb4db890024d84ddb
Zaur Dedegkaev <dedegkaev.z@gmail.com>
2016-04-07 17:27:20 +0300

    add browser-sync

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -13,6 +13,8 @@ var connect = require('gulp-connect');
 var less = require('gulp-less');
 var annotate = require('gulp-ng-annotate');
 var useref = require('gulp-useref');
+var browserSync = require('browser-sync');
+var browserSyncReload = browserSync.reload;
 
 
 // var gCheerio = require('gulp-cheerio');
@@ -40,12 +42,14 @@ options.less = {
 options.hrml = {
 
 };
-
 options.server = {
 	development: {
 		root: options.main.path.tmp,
 		port: 8081,
-		livereload: true
+		livereload: true,
+        middleware: function(connect) {
+            return [connect().use('/bower_components', connect.static('bower_components'))];
+        }
 	},
 	production: {
 		root: options.main.path.dist,
@@ -58,7 +62,14 @@ var files = {
     index: options.main.path.dev+'/index.html',
     html: options.main.path.dev+'/**/*.html',
     less: options.main.path.dev+'/**/*.less',
+    appless: options.main.path.dev+'/app.less',
 };
+
+// CLEAN
+gulp.task('clean:dev', function() {
+    return gulp.src(options.main.path.tmp)
+    .pipe(clean());
+});
 
 // HTML
 gulp.task('html:dev', function() {
@@ -70,11 +81,12 @@ gulp.task('html:dist', function() {
     console.log('html');
 });
 
+
 // LESS
 gulp.task('less:dev', function() {
-	return gulp.src(files.less)
+	return gulp.src(files.appless)
     .pipe(less())
-    .pipe(gulp.dest('./'+options.main.path.tmp+'/styles'));
+    .pipe(gulp.dest('./'+options.main.path.tmp));
 });
 
 gulp.task('less:dist', function() {
@@ -84,13 +96,18 @@ gulp.task('less:dist', function() {
     .pipe(gulp.dest('./'+options.main.path.dist+'/styles'));
 });
 
-gulp.task('connect:dev', function() {
-    connect.server(options.server.development);
+gulp.task('browser-sync', function() {
+    browserSync({
+		server: {
+			baseDir: "./app",
+            routes: {
+                "/bower_components": "bower_components"
+            }
+		}
+	});
 });
 
-gulp.task('connect:dist', function() {
-    connect.server(options.server.production);
-});
+
 
 gulp.task('watch', function() {
     var htmlwatcher = gulp.watch(files.html, ['html:dev']);
@@ -105,6 +122,6 @@ gulp.task('watch', function() {
     });
 });
 
-gulp.task('dev', ['less:dev', 'html:dev', 'connect:dev', 'watch']);
+gulp.task('dev', ['clean:dev', 'less:dev', 'html:dev', 'connect:dev', 'watch']);
 gulp.task('build', ['less:dist', 'html:dist', 'connect:dist', 'watch']);
 gulp.task('default', ['dev']);
