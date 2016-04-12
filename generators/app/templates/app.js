036269de845d63dc26f66cdf257edf224f17fb02
Zaur Dedegkaev <dedegkaev.z@gmail.com>
2016-04-12 15:31:11 +0300

    main module tempalte fixes

--- generators/app/templates/app.js
+++ generators/app/templates/app.js
@@ -1,22 +1,14 @@
-angular.module('<%= appname %>', ['ui.bootstrap','<%= routerModuleName %>','ngAnimate']);
-
+angular.module('<%= appname %>', ['ui.bootstrap', '<%= routerModuleName %>', '<%= requestsSvcName %>', 'ngAnimate']);
 <% if (!uirouter) { %>
-angular.module('<%= appname %>').config(function($routeProvider) {
-
+angular.module('<%= appname %>').config(function($routeProvider, $locationProvider) {
     $routeProvider.otherwise({redirectTo:'/'});
-
+    $locationProvider.html5Mode(true);
 });
-<% } %>
-
-<% if (uirouter) { %>
+<% } %><% if (uirouter) { %>
 angular.module('<%= appname %>').config(function($stateProvider, $urlRouterProvider) {
-
     $urlRouterProvider.otherwise('/');
-
 });
 <% } %>
-
-
 angular.module('<%= appname %>').run(function($rootScope) {
 <% if (uirouter) { %>
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
